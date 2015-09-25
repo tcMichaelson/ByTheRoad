c758a0615f3f7168a21663eddc906d5b52f8e1ba
tcMichaelson <tc.michaelson@gmail.com>
2015-09-25 16:29:58 -0500

    initial

---
+++ bytheroad/ByTheRoad/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ByTheRoad.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}

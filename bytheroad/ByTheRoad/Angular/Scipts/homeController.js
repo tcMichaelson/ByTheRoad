82067ae344049e90b7bacf6af3e482c4d5ef55d5
jdianala <jdianala@yahoo.com>
2015-09-28 11:14:02 -0500

    created homeController with self login

---
+++ bytheroad/ByTheRoad/Angular/Scipts/homeController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    angular
+        .module('byTheRoad')
+        .controller('homeController', function ($location, $http) {
+            var self = this;
+            self.login = function () {
+                $http.post('/token', "grant_type=password&username=" + self.username + "&password=" + self.password,
+                    {
+                        hearders: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    })
+                .success(function (data) {
+                    token = data.access_token;
+                    $http.defaults.headers.common['Authorization'] = 'bearer ' + token;
+                    $location.path('/list');
+                })
+                .error(function () {
+                    con.error('Error loggin in.');
+                });
+            };
+        });
+})();

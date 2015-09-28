d71098de2e3e818209719ccb7349efbda6ce36da
jdianala <jdianala@yahoo.com>
2015-09-28 18:11:37 -0500

    merge registered function into home controller

--- bytheroad/ByTheRoad/Angular/scripts/homeController.js
+++ bytheroad/ByTheRoad/Angular/scripts/homeController.js
@@ -3,6 +3,7 @@
         .module('byTheRoad')
         .controller('homeController', function ($location, $http) {
             var self = this;
+
             self.login = function () {
                 $http.post('/token', "grant_type=password&username=" + self.username + "&password=" + self.password,
                     {
@@ -13,11 +14,16 @@
                 .success(function (data) {
                     token = data.access_token;
                     $http.defaults.headers.common['Authorization'] = 'bearer ' + token;
-                    $location.path('/list');
                 })
                 .error(function () {
                     console.error('Error loggin in.');
                 });
             };
+
+            self.register = function () {
+                roadService.register(self);
+            };
+
         });
+
 })();

c2fb178348826794fab56d9958714d8c0825499f
Christine Cazares <3cccazares@gmail.com>
2015-09-28 09:49:33 -0500

    poi class

---
+++ bytheroad/ByTheRoad/Models/PointOfIntsterset .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByTheRoad.Models
+{
+    public class PointOfIntsterset
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}

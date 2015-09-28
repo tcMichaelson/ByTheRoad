67c64bbcbf1095aac1e3d99614a2a23c33362e03
jdianala <jdianala@yahoo.com>
2015-09-28 15:16:23 -0500

    retrieve previous version

---
+++ bytheroad/ByTheRoad/Angular/Scripts/roadService.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    angular
+    .module('byTheRoad')
+    .service('roadService', ['$resource', 'routeUrls', '$route', '$location', function ($resource, routeUrls, $route, $location) {
+        var Register = $resource(routeUrls.registerApi, {}, {});
+        var self = this;
+
+        self.register = function (user) {
+            new Register(user).$save(function(data) {
+                console.log(data);
+            })
+        }
+    }]);
+})();

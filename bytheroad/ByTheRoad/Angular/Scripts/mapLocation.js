a8ea20506081fc97e54efbf16840c6b3983fe69f
jdianala <jdianala@yahoo.com>
2015-09-28 21:16:17 -0500

    edited nearby and search by text

--- bytheroad/ByTheRoad/Angular/Scripts/mapLocation.js
+++ bytheroad/ByTheRoad/Angular/Scripts/mapLocation.js
@@ -71,10 +71,10 @@ function ReCenter(map) {
 //Nearby Search Function
 function nearby(long, lat)
 {
-    function initialize(long, lat, radius, type) {
+    function initialize(long, lat, radius, id, type) {
         var pyrmont = new google.maps.LatLng(long, lat);
 
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById(id), {
             center: pyrmont,
             zoom: 15
         });
@@ -105,18 +105,18 @@ function nearby(long, lat)
 function textSearch(long, lat)
 {
     function initialize() {
-        var pyrmont = new google.maps.LatLng(long, lat);
+        var pyrmont = new google.maps.LatLng(long, lat, id, radius, userQuery);
 
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById(id), {
             center: pyrmont,
             zoom: 15
         });
 
         var request = {
             location: pyrmont,
-            radius: '500',
-            query: 'restaurant'
-        };
+            radius: radius,
+            query: userQuery
+                    };
 
         service = new google.maps.places.PlacesService(map);
         service.textSearch(request, callback);

3175b45f0718d54906ba170c5b0d6325711684c3
jdianala <jdianala@yahoo.com>
2015-09-30 17:06:13 -0500

    updated search box

--- bytheroad/ByTheRoad/Angular/Scripts/mapService.js
+++ bytheroad/ByTheRoad/Angular/Scripts/mapService.js
@@ -25,6 +25,73 @@
 
         self.initTextSearch = function () {
             console.log(locHist);
+            var currentLoc = { lat: locHist[0].lat, lng: locHist[0].lng };
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: currentLoc,
+                zoom: 13
+            });
+
+            // Create the search box and link it to the UI element.
+            var input = document.getElementById('textsearch');
+            var searchBox = new google.maps.places.SearchBox(input);
+
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener('bounds_changed', function () {
+                searchBox.setBounds(map.getBounds());
+            });
+
+            var markers = [];
+            // [START region_getplaces]
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener('places_changed', function() {
+                var places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+
+                // Clear out the old markers.
+                markers.forEach(function(marker) {
+                    marker.setMap(null);
+                });
+                markers = [];
+
+                // For each place, get the icon, name and location.
+                var bounds = new google.maps.LatLngBounds();
+                places.forEach(function(place) {
+                    var icon = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                    };
+
+                    // Create a marker for each place.
+                    markers.push(new google.maps.Marker({
+                        map: map,
+                        icon: icon,
+                        title: place.name,
+                        position: place.geometry.location
+                    }));
+
+                    if (place.geometry.viewport) {
+                        // Only geocodes have viewport.
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        regTextSearch();
+                    }
+                });
+                map.fitBounds(bounds);
+            });
+            // [END region_getplaces]
+        }
+
+
+       function regTextSearch () {
+            console.log(locHist);
             var pyrmont = { lat: locHist[0].lat, lng: locHist[0].lng };
 
             map = new google.maps.Map(document.getElementById('map'), {

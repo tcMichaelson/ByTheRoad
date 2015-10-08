e8758212ea9990953fa0303f19c78f26295e1b10
Christine Cazares <3cccazares@gmail.com>
2015-10-08 12:00:00 -0500

    errorlogin fix

--- bytheroad/ByTheRoad/Angular/scripts/homeController.js
+++ bytheroad/ByTheRoad/Angular/scripts/homeController.js
@@ -55,7 +55,7 @@
                 })
                 .error(function () {
                     console.error('Error loggin in.');
-
+                    self.loginError = true;
                 });
             };
 

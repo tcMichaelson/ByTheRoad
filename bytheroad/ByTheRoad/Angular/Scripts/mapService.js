00f0b82befed6710193154585d4c414d843f7c9f
tcMichaelson <tc.michaelson@gmail.com>
2015-10-13 11:17:39 -0500

    trying to fix distance

--- bytheroad/ByTheRoad/Angular/Scripts/mapService.js
+++ bytheroad/ByTheRoad/Angular/Scripts/mapService.js
@@ -6,6 +6,7 @@
             var infowindow;
             var self = this;
             self.places = [];
+            self.distancesFound = 0;
             var placeIdArray = [];
             var searchCircle;
             var markers = [];
@@ -147,6 +148,11 @@
                 }, 1000);
             }
 
+            self.reCenterCustom = function (pos, zoom) {
+                map.panTo(pos);
+                map.setZoom(zoom);
+            }
+
             //grabbing the info for each place
             self.callback = function (places, status) {
                 if (markers[0]) {
@@ -187,6 +193,7 @@
                                 self.results[idx].route = response;
                                 self.results[idx].distance = response.routes[0].legs[0].distance.text;
                                 console.log("placeDist: ", placeDist)
+                                self.distancesFound++;
                             });
                             resultIdx++;
                         }
--- bytheroad/ByTheRoad/Angular/scripts/homeController.js
+++ bytheroad/ByTheRoad/Angular/scripts/homeController.js
@@ -180,7 +180,7 @@
 
             self.startInterval = function (func, searchPos) {
                 var checkResults = window.setInterval(function () {
-                    if (self.results[0] === 'none' && queryLimit < 10) {
+                    if (self.results[0] === 'none' && queryLimit < 20) {
                         self.runSearch(func, queryLimit);
                         if (queryLimit > 0){
                             queryLimit *= -1;
@@ -193,12 +193,15 @@
                         self.getResults();
                         $scope.$apply();
                     } else {
+                        
+                        $scope.$apply(function () {
+                            self.showResultsBox();
+                            mapService.reCenter();
+                        });
+                        mapService.distancesFound = 0;
                         queryLimit = 1;
-                        self.showResultsBox();
-                        mapService.reCenter();
-                        console.log(self.results);
+                        console.log("one distance: ", self.results[0].distance);
                         clearInterval(checkResults);
-                        $scope.$apply();
                     }
                 }, 100);
             }
@@ -219,6 +222,7 @@
                 mapService.places = self.places;
                 func(self, searchPos);//Invoked func
                 if (offset === 0) {
+                    mapService.reCenterCustom(searchPos, 13);
                     self.startInterval(func, searchPos);
                 }
             }
@@ -249,10 +253,14 @@
                     }
                     mapService.callback(places, google.maps.places.PlacesServiceStatus.OK);
                     self.startInterval();
-                    self.showResultsBox();
 
                 });
             };
+
+            self.hideResultsBox = function () {
+                self.animationResults = "animated slideOutLeft";
+                self.animationSaved = "animated slideOutLeft";
+            }
 
             self.showResultsBox = function () {
                 self.animationResults = "animated slideInLeft";

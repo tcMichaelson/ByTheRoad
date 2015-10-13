ecea4629460c608be2b07dcee0f110104fe06d5a
reenu saini <reenu.saini@outlook.com>
2015-10-13 14:16:33 -0500

    fixed merges

--- bytheroad/ByTheRoad/Migrations/201510131908422_initial.Designer.cs
+++ bytheroad/ByTheRoad/Migrations/201510131908422_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace ByTheRoad.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201510130234020_init"; }
+            get { return "201510131908422_initial"; }
         }
         
         string IMigrationMetadata.Source

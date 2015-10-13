5a2931cabb32f14135f38b423677286288a2e0d2
tcMichaelson <tc.michaelson@gmail.com>
2015-10-13 16:35:19 -0500

    changed logout function

--- bytheroad/ByTheRoad/Migrations/201510132053573_initial.Designer.cs
+++ bytheroad/ByTheRoad/Migrations/201510132053573_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace ByTheRoad.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class intai : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(intai));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201510131852286_intai"; }
+            get { return "201510132053573_initial"; }
         }
         
         string IMigrationMetadata.Source
